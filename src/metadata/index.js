578cbabd32026fe2802e9b3fba2cb13f077e9cde
unpete <info@oknosoft.ru>
2020-02-11 22:52:30 +0500

    печать без dhtmlx

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -4,6 +4,7 @@ import MetaEngine from 'metadata-core';
 import plugin_pouchdb from 'metadata-pouchdb';
 import plugin_ui from 'metadata-abstract-ui';
 import plugin_ui_tabulars from 'metadata-abstract-ui/tabulars';
+import plugin_mime from 'metadata-core/lib/mime.min';
 import plugin_react from 'metadata-react/plugin';
 import proxy_login, {load_ram, load_common} from 'metadata-superlogin/proxy';
 
@@ -28,6 +29,7 @@ import {metaActions, metaMiddleware} from 'metadata-redux';
 MetaEngine
   .plugin(plugin_pouchdb)     // подключаем pouchdb-адаптер к прототипу metadata.js
   .plugin(plugin_ui)          // подключаем общие методы интерфейса пользователя
+  .plugin(plugin_mime)        // подключаем mime-types
   .plugin(plugin_ui_tabulars) // подключаем методы экспорта табличной части
   .plugin(plugin_react);      // подключаем react-специфичные модификаторы к scheme_settings
 
@@ -40,6 +42,9 @@ patch_cnn();
 
 // со скрипом инициализации метаданных, так же - не затягиваем
 meta_init($p);
+
+// сумма прописью
+import('metadata-abstract-ui/rubles');
 
 // скрипт инициализации в привязке к store приложения
 export function init(store) {
