24a29b3b01cc45fef16957b9947a156e82ab94f4
unpete <info@oknosoft.ru>
2023-02-24 19:09:18 +0500

    каркас приложения

--- src/components/FrmLogin/Root.js
+++ src/components/FrmLogin/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 
-export const LoginRoot = styled('div')(({theme}) => ({
+export const LoginRoot = styled('form')(({theme}) => ({
   [theme.breakpoints.up('md')]: {
     marginLeft: '10vw',
     marginTop: '10vh',
