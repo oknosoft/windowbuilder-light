babfbe038980be023be061e908fca2f393a924e6
unpete <info@oknosoft.ru>
2023-03-11 15:21:20 +0500

    структура формы объекта

--- src/metadata/doc/calc_order/Obj.js
+++ src/metadata/doc/calc_order/Obj.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListSubheader from '@mui/material/ListSubheader';
+import {useParams, useNavigate} from 'react-router-dom';
+import {useTitleContext} from '../../../components/App';
 
+const title = {title: 'Расчёт-заказ', appTitle: <Typography variant="h6" noWrap>Расчёт-заказ</Typography>};
 export default function CalcOrderObj() {
-  return "CalcOrderObj";
+
+  const [obj, setObj] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  const params = useParams();
+  const {setTitle} = useTitleContext();
+
+
+  React.useEffect(() => {
+    const {ref} = params;
+    setTitle(title);
+  }, []);
+
+  return <List>
+    <ListSubheader>
+      Toolbar документа
+    </ListSubheader>
+    <ListItem>
+      Шапка
+    </ListItem>
+    <ListSubheader>
+      Варианты табчасти
+    </ListSubheader>
+    <ListItem>
+      Табчасть
+    </ListItem>
+  </List>;
 }
