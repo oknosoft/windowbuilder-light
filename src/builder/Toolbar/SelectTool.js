ae8ffad0cf73e902fc2f35e45bd9fe5eaf1ff085
unpete <info@oknosoft.ru>
2023-10-04 00:16:20 +0500

    иконы инструментов + версия core

--- src/builder/Toolbar/SelectTool.js
+++ src/builder/Toolbar/SelectTool.js
@@ -3,9 +3,10 @@ import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import PhoneIcon from '@mui/icons-material/Phone';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import PersonPinIcon from '@mui/icons-material/PersonPin';
+import CursorIcon from '../../_common/styles/icons/Cursor';
+import PenIcon from '../../_common/styles/icons/Pen';
+import ZoomFitIcon from '../../_common/styles/icons/ZoomFit';
+import VideoSettingsIcon from '@mui/icons-material/VideoSettings';
 import {Toolbar, HtmlTooltip} from '../../_common/App/styled';
 
 export default function SelectTool() {
@@ -15,14 +16,16 @@ export default function SelectTool() {
     setTool(newValue);
   };
   return <Toolbar>
+    <HtmlTooltip title="Вписать в окно (масштаб)">
+      <IconButton onClick={null}><ZoomFitIcon/></IconButton>
+    </HtmlTooltip>
     <Tabs value={tool} onChange={handleChange} >
-      <Tab value={0} icon={<PhoneIcon />} aria-label="phone" />
-      <Tab value={1} icon={<FavoriteIcon />} aria-label="favorite" />
-      <Tab value={2} icon={<PersonPinIcon />} aria-label="person" />
+      <Tab value={0} icon={<HtmlTooltip title="Выделить и сдвинуть"><CursorIcon /></HtmlTooltip>} aria-label="select" />
+      <Tab value={1} icon={<HtmlTooltip title="Нарисовать профиль"><PenIcon /></HtmlTooltip>} aria-label="draw" />
     </Tabs>
     <Typography sx={{flex: 1}}></Typography>
-    <HtmlTooltip title="Настройки">
-      <IconButton onClick={null}><PersonPinIcon/></IconButton>
+    <HtmlTooltip title="Настройки отображения">
+      <IconButton onClick={null}><VideoSettingsIcon/></IconButton>
     </HtmlTooltip>
   </Toolbar>
 }
