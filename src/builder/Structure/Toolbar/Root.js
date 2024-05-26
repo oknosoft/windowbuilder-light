e3350e43eb20c149dd6cfbacf52ffaec0b976446
unpete <info@oknosoft.ru>
2024-05-26 22:19:52 +0500

    шрифты и стили

--- src/builder/Structure/Toolbar/Root.js
+++ src/builder/Structure/Toolbar/Root.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Toolbar} from '../../../aggregate/App/styled';
 
 export default function RootToolbar() {
-  return <Toolbar>ProductStructure</Toolbar>
+  return <Toolbar style={{fontFamily: 'GOST type B'}}>ProductStructure</Toolbar>
 }
