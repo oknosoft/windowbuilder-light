e90e0cd173bf8b4ff3b5c9a3fa9eb3f74e04fec4
unpete <info@oknosoft.ru>
2025-04-24 17:02:13 +0500

    вставки заполнений

--- src/builder/Controls/Panel/FillingProps.js
+++ src/builder/Controls/Panel/FillingProps.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import FieldInsetProfile from '../../DataField/InsetProfile';
+
+const {dp, utils} = $p;
+export const {fields} =  dp.builderPen.metadata();
 
 export default function FillingProps({layer, elm}) {
   return <>
     {`Заполнение ${elm.index}`}
+    <FieldInsetProfile obj={elm} fld="inset" meta={fields.inset}/>
   </>;
 }
