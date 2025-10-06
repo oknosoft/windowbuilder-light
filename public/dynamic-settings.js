1248484e8e56eaa17516f32685d9bd988e180270
unpete <info@oknosoft.ru>
2025-10-06 21:01:08 +0500

    paper

--- public/dynamic-settings.js
+++ public/dynamic-settings.js
@@ -12,18 +12,18 @@
   window._dynamic_patch_ = {
 
     predefined: {
-      zone: 92,
+      zone: 32,
       keys: keys21,
     },
 
   };
 
   // заглушка
-  try {
-    sessionStorage.setItem('zone', window._dynamic_patch_.predefined.zone);
-    sessionStorage.setItem('year', 2025);
-  }
-  catch (e) {}
+  // try {
+  //   sessionStorage.setItem('zone', window._dynamic_patch_.predefined.zone);
+  //   sessionStorage.setItem('year', 2025);
+  // }
+  // catch (e) {}
   // при желании-необходимости, здесь можно разместить...
 
 })();
