6fdb52cc91d87e2e557bb3400e254336c7700883
unpete <info@oknosoft.ru>
2026-08-21 11:46:29 +0300

    этикетки обрези

--- src/documents/work_centers_task/print/StickerCut.js
+++ src/documents/work_centers_task/print/StickerCut.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function StickerCut({row, cssName}) {
   const {nom, len, width} = row;
   return <article>
-    <div className={`partner${cssName ? ' partner-nowrap' : ' nowrap'}`}>{nom.name}</div>
+    <div className="partner nowrap">{nom.name}</div>
     <div className="cut">{len}</div>
     <div className="cut">{width}</div>
   </article>;
